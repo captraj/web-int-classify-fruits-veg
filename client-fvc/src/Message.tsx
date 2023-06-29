9d948ae40b94a85418f74f630a5714198cf4ead1
captraj <76588598+captraj@users.noreply.github.com>
2023-06-30 00:26:23 +0530

    set up basic App ts for adding a placeholder

---
+++ client-fvc/src/Message.tsx
@@ -0,0 +1,9 @@
+
+//PascalCase
+function Message() {
+    //  JSX: JavaScript XML
+    const message = "This is a website that is going to be the interface for the fruits and vegetable classification service";
+    return <h1>{message}</h1>;
+}
+
+export default Message;
